55b7916bb88185390213f74f80078d1d10e6ec5b
Pathuri Mounika <mounikapathuri30@gmail.com>
2025-09-22 11:41:27 +0530

    commit

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 def home():
     return "Hello from Flask + Docker!"
 
+@app.route('/hello/<name>')
+def hello_name(name):
+    return f"Hello, {name}!"
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
